4336c4f9418cf32c33a5ca1f89893d46f67eb84f
Lisa Gacura <lisa.lennox@clarustech.com>
2025-10-24 14:02:59 -0400

    First Commit

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -4,8 +4,20 @@ import Link from 'next/link';
 
 const items = [
   {
-    title: 'Get started',
-    links: [{href: '/docs', children: 'Overview'}],
+    title: 'Overview',
+    links: [{href: '/docs', children: 'Summary'}],
+  },
+  {
+    title: 'Contents',
+    links: [{href: '/docs', children: 'Sample 1'}],
+  },
+  {
+    title: 'Claim',
+    links: [{href: '/Claim/Submission/Input', children: 'Claim Submission Input'}, {href: '/Claim/Submission/Output', children: 'Claim Submission Output'}],
+  },
+    {
+    title: 'Gates',
+    links: [{href: '/Gates', children: 'Canadian Gate Territory'}, {href: '/Gates', children: 'Diagnosis Gate'}, {href: '/Gates', children: 'Discounted Item Gate'}, {href: '/Gates', children: 'Market Channel Code Brand Gate'}, {href: '/Gates', children: 'Multi Pet Invoice Gate'}, {href: '/Gates', children: 'Species Gate'}, {href: '/Gates', children: 'State Gate'}, {href: '/Gates', children: 'Symptom Date Check Gate'}, {href: '/Gates', children: 'Vet Missing Gate'}, {href: '/Gates', children: 'Version Gate'},], 
   },
 ];
 
